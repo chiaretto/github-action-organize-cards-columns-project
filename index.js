74727a052bb02d116134d9106db61737ac36347f
fabianofernandeszup <fabiano.fernandes@zup.com.br>
2023-03-25 19:25:42 -0300

    Finish v1
    Signed-off-by: fabianofernandeszup <fabiano.fernandes@zup.com.br>

--- index.js
+++ index.js
@@ -1,74 +1,140 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { graphql } = require("@octokit/graphql");
+const { Repository } = require("@octokit/graphql-schema");
 
 // Toolkit docs: https://github.com/actions/toolkit
 async function run() {
   try {
 
-    // const inputs = {
-    //   token: core.getInput('github-token', {required: true})
-    // };
     const inputs = {
-      token: "",
-      org: "chiarettos",
-      repo: ""
-
-    };
-
-    const client = new github.GitHub(inputs.token);
-
-    let body;
-    let prNumber;
-
-    if (inputs.prNumberForced) {
-        prNumber = inputs.prNumberForced
-        const responsePr = await client.pulls.get({
-            owner: github.context.repo.owner,
-            repo: github.context.repo.repo,
-            pull_number: prNumber,
-        })
-        body = responsePr.data.body
-    } else if (github.context.payload.pull_request && github.context.payload.pull_request.number) {
-        prNumber = github.context.payload.pull_request.number
-        body = github.context.payload.pull_request.body;
-    } else if (github.context.payload.workflow_run.pull_requests && github.context.payload.workflow_run.pull_requests) {
-        prNumber = github.context.payload.workflow_run.pull_requests.shift().number
-        const responsePr = await client.pulls.get({
-          owner: github.context.repo.owner,
-          repo: github.context.repo.repo,
-          pull_number: prNumber,
-        })
-        body = responsePr.data.body
-    }
-
-    console.log('PR Number: ', prNumber);
-
-    console.log('Original description: ', body);
-
-    if (!body) return;
-
-    const separator = "\r\r---------------------------------------------\r\r"
-    const dateTime = "\r\r> Generated at " + Date()
-
-    let newBody;
-    if (inputs.replaceLastMessage) {
-      newBody = body.split(separator).shift() + separator + inputs.message + dateTime;
-    } else {
-      newBody = body + separator + inputs.message + dateTime;
-    }
-
-    console.log('Concatenated description: ', newBody);
-
-    const response = await client.pulls.update({
+      token: core.getInput('github-token', {required: true}),
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
-      pull_number: prNumber,
-      body: newBody
-    });
+    };
 
-    if (response.status !== 200) {
-      core.error('There was an issue while trying to update the pull-request.');
+    // Get Projects From Repo
+    const { repository } = await graphql(
+        `
+        {
+        repository(owner: "`+inputs.owner+`", name: "`+inputs.repo+`") {
+          projectsV2(first: 20) {
+            nodes {
+              id
+              title
+            }
+          }
+        }
+        }
+        `,
+        {
+          headers: {
+            authorization: `token `+inputs.token,
+          },
+        }
+    );
+
+    // Get Card Of Project
+    for (project of repository.projectsV2.nodes) {
+      console.log("Project Title:", project.title)
+      let perPage = 50
+      let hasNextPage = false
+      let endCursor = ''
+      do {
+        const query = `{
+              node(id: "` + project.id + `") {
+                ... on ProjectV2 {
+                  title
+                  id
+                  items(first: `+perPage+endCursor+`) {
+                    edges {
+                      node {
+                        id
+                        type
+                        fieldValueByName(name: "Status") {
+                          ... on ProjectV2ItemFieldSingleSelectValue {
+                            name
+                          }
+                        }
+                        content {
+                          ... on PullRequest {
+                            id
+                            title
+                          }
+                          ... on Issue {
+                            id
+                            title
+                          }
+                          ... on DraftIssue {
+                            id
+                            title
+                          }
+                        }
+                      }
+                    }
+                    pageInfo {
+                      hasNextPage
+                      endCursor
+                    }
+                  }
+                }
+              }
+            }`
+        // console.log(query)
+        const {node} = await graphql(
+            query,
+            {
+              headers: {
+                authorization: `token ` + inputs.token,
+              },
+            }
+        );
+        let mutations = []
+        for (item of node.items.edges) {
+          // console.log(item)
+          if (item.node?.fieldValueByName?.name) {
+            let titleStatusSplited = item.node?.fieldValueByName?.name.split('-')
+            if (titleStatusSplited[0].trim().length <= 2) {
+              let iconStatus = titleStatusSplited[0].trim()
+              let titleCardSplited = item.node.content.title.split('-')
+              let newTitleCard = iconStatus + ' ' +  titleCardSplited.join(' - ').replace(/[^a-zA-Z0-9_\(\)\[\]\- ]/g, '').replace(/  +/g, ' ').trim()
+              console.log('['+item.node.type+'] ('+item.node.id+') oldtitleCard:', item.node.content.title, ' | newTitleCard:', newTitleCard)
+
+              let mutation = ''
+              switch (item.node.type) {
+                case 'ISSUE':
+                  mutation =`MyMutation`+item.node.content.id+`: updateIssue(input: {id: "`+item.node.content.id+`", title: "`+newTitleCard+`"}) {clientMutationId}`
+                  break;
+                case 'DRAFT_ISSUE':
+                  mutation =`MyMutation`+item.node.content.id+`: updateProjectV2DraftIssue(input: {draftIssueId: "`+item.node.content.id+`", title: "`+newTitleCard+`"}) {clientMutationId}`
+                  mutations.push(mutation)
+                  break;
+                case 'PULL_REQUEST':
+                  mutation = `MyMutation`+item.node.content.id+`: updatePullRequest(input: {pullRequestId: "`+item.node.content.id+`", title: "`+newTitleCard+`"}) {clientMutationId}`
+                  mutations.push(mutation)
+                  break;
+              }
+            }
+          }
+        }
+        if (mutations.length) {
+          // console.log(mutations.join('\n'))
+          const clientMutationId = await graphql(
+              `mutation {` + mutations.join('\n') + `}`
+              ,
+              {
+                headers: {
+                  authorization: `token ` + inputs.token,
+                },
+              }
+          );
+          // console.log(clientMutationId)
+        }
+        hasNextPage = node.items.pageInfo.hasNextPage
+        endCursor = `, after: "`+node.items.pageInfo.endCursor+`"`
+      } while (hasNextPage)
     }
+    console.log("Done")
   } catch (error) {
     core.error(error);
     core.setFailed(error.message);
