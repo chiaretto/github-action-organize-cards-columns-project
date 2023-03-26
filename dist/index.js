a1d9c522354843e7df109cf114c187938dd2f1ff
fabianofernandeszup <fabiano.fernandes@zup.com.br>
2023-03-26 10:45:42 -0300

    Fix action
    Signed-off-by: fabianofernandeszup <fabiano.fernandes@zup.com.br>

--- dist/index.js
+++ dist/index.js
@@ -56237,9 +56237,6 @@ async function run() {
       repo: github.context.repo.repo,
     };
 
-    console.log('inputs.owner:', inputs.owner)
-    console.log('inputs.repo:', inputs.repo)
-
     // Get Projects From Repo
     let queryRepo = `{
         repository(owner: "`+inputs.owner+`", name: "`+inputs.repo+`") {
@@ -56251,7 +56248,7 @@ async function run() {
           }
         }
         }`
-    console.log(queryRepo)
+    // console.log(queryRepo)
     const { repository } = await graphql(
         queryRepo
         ,
@@ -56262,109 +56259,112 @@ async function run() {
         }
     );
     console.log(repository)
-
-    // Get Card Of Project
-    for (project of repository.projectsV2.nodes) {
-      console.log("Project Title:", project.title)
-      let perPage = 50
-      let hasNextPage = false
-      let endCursor = ''
-      do {
-        const query = `{
-              node(id: "` + project.id + `") {
-                ... on ProjectV2 {
-                  title
-                  id
-                  items(first: `+perPage+endCursor+`) {
-                    edges {
-                      node {
-                        id
-                        type
-                        fieldValueByName(name: "Status") {
-                          ... on ProjectV2ItemFieldSingleSelectValue {
-                            name
+    if (repository?.projectsV2?.nodes?.length) {
+      // Get Card Of Project
+      for (project of repository.projectsV2.nodes) {
+        console.log("Project Title:", project.title)
+        let perPage = 50
+        let hasNextPage = false
+        let endCursor = ''
+        do {
+          const query = `{
+                node(id: "` + project.id + `") {
+                  ... on ProjectV2 {
+                    title
+                    id
+                    items(first: ` + perPage + endCursor + `) {
+                      edges {
+                        node {
+                          id
+                          type
+                          fieldValueByName(name: "Status") {
+                            ... on ProjectV2ItemFieldSingleSelectValue {
+                              name
+                            }
                           }
-                        }
-                        content {
-                          ... on PullRequest {
-                            id
-                            title
-                          }
-                          ... on Issue {
-                            id
-                            title
-                          }
-                          ... on DraftIssue {
-                            id
-                            title
+                          content {
+                            ... on PullRequest {
+                              id
+                              title
+                            }
+                            ... on Issue {
+                              id
+                              title
+                            }
+                            ... on DraftIssue {
+                              id
+                              title
+                            }
                           }
                         }
                       }
-                    }
-                    pageInfo {
-                      hasNextPage
-                      endCursor
+                      pageInfo {
+                        hasNextPage
+                        endCursor
+                      }
                     }
                   }
                 }
-              }
-            }`
-        // console.log(query)
-        const {node} = await graphql(
-            query,
-            {
-              headers: {
-                authorization: `token ` + inputs.token,
-              },
-            }
-        );
-        let mutations = []
-        for (let item of node.items.edges) {
-          // console.log(item)
-          if (item.node?.fieldValueByName?.name) {
-            let titleStatusSplited = item.node?.fieldValueByName?.name.split('-')
-            if (titleStatusSplited[0].trim().length <= 2) {
-              let iconStatus = titleStatusSplited[0].trim()
-              let titleCardSplited = item.node.content.title.split('-')
-              let newTitleCard = iconStatus + ' ' +  titleCardSplited.join(' - ').replace(/[^a-zA-Z0-9_\(\)\[\]\- ]/g, '').replace(/  +/g, ' ').trim()
-              console.log('['+item.node.type+'] ('+item.node.id+') oldtitleCard:', item.node.content.title, ' | newTitleCard:', newTitleCard)
-              if (item.node.content.title !== newTitleCard) {
-                let mutation = ''
-                switch (item.node.type) {
-                  case 'ISSUE':
-                    mutation = `MyMutation` + item.node.content.id + `: updateIssue(input: {id: "` + item.node.content.id + `", title: "` + newTitleCard + `"}) {clientMutationId}`
-                    break;
-                  case 'DRAFT_ISSUE':
-                    mutation = `MyMutation` + item.node.content.id + `: updateProjectV2DraftIssue(input: {draftIssueId: "` + item.node.content.id + `", title: "` + newTitleCard + `"}) {clientMutationId}`
-                    mutations.push(mutation)
-                    break;
-                  case 'PULL_REQUEST':
-                    mutation = `MyMutation` + item.node.content.id + `: updatePullRequest(input: {pullRequestId: "` + item.node.content.id + `", title: "` + newTitleCard + `"}) {clientMutationId}`
-                    mutations.push(mutation)
-                    break;
-                }
-              }
-            }
-          }
-        }
-        if (mutations.length) {
-          // console.log(mutations.join('\n'))
-          const clientMutationId = await graphql(
-              `mutation {` + mutations.join('\n') + `}`
-              ,
+              }`
+          // console.log(query)
+          const {node} = await graphql(
+              query,
               {
                 headers: {
                   authorization: `token ` + inputs.token,
                 },
               }
           );
-          // console.log(clientMutationId)
-        }
-        hasNextPage = node.items.pageInfo.hasNextPage
-        endCursor = `, after: "`+node.items.pageInfo.endCursor+`"`
-      } while (hasNextPage)
+          let mutations = []
+          for (let item of node.items.edges) {
+            // console.log(item)
+            if (item.node?.fieldValueByName?.name) {
+              let titleStatusSplited = item.node?.fieldValueByName?.name.split('-')
+              if (titleStatusSplited[0].trim().length <= 2) {
+                let iconStatus = titleStatusSplited[0].trim()
+                let titleCardSplited = item.node.content.title.split('-')
+                let newTitleCard = iconStatus + ' ' + titleCardSplited.join(' - ').replace(/[^a-zA-Z0-9_\(\)\[\]\- ]/g, '').replace(/  +/g, ' ').trim()
+                console.log('[' + item.node.type + '] (' + item.node.id + ') oldtitleCard:', item.node.content.title, ' | newTitleCard:', newTitleCard)
+                if (item.node.content.title !== newTitleCard) {
+                  let mutation = ''
+                  switch (item.node.type) {
+                    case 'ISSUE':
+                      mutation = `MyMutation` + item.node.content.id + `: updateIssue(input: {id: "` + item.node.content.id + `", title: "` + newTitleCard + `"}) {clientMutationId}`
+                      break;
+                    case 'DRAFT_ISSUE':
+                      mutation = `MyMutation` + item.node.content.id + `: updateProjectV2DraftIssue(input: {draftIssueId: "` + item.node.content.id + `", title: "` + newTitleCard + `"}) {clientMutationId}`
+                      mutations.push(mutation)
+                      break;
+                    case 'PULL_REQUEST':
+                      mutation = `MyMutation` + item.node.content.id + `: updatePullRequest(input: {pullRequestId: "` + item.node.content.id + `", title: "` + newTitleCard + `"}) {clientMutationId}`
+                      mutations.push(mutation)
+                      break;
+                  }
+                }
+              }
+            }
+          }
+          if (mutations.length) {
+            // console.log(mutations.join('\n'))
+            const clientMutationId = await graphql(
+                `mutation {` + mutations.join('\n') + `}`
+                ,
+                {
+                  headers: {
+                    authorization: `token ` + inputs.token,
+                  },
+                }
+            );
+            // console.log(clientMutationId)
+          }
+          hasNextPage = node.items.pageInfo.hasNextPage
+          endCursor = `, after: "` + node.items.pageInfo.endCursor + `"`
+        } while (hasNextPage)
+      }
+      console.log("Done")
+    } else {
+      console.log("Projects Not Found!")
     }
-    console.log("Done")
   } catch (error) {
     core.error(error);
     core.setFailed(error.message);
