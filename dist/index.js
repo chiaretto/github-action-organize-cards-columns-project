82ad83547b3e078d4a0484c1759ee023a4aaa89c
fabianofernandeszup <fabiano.fernandes@zup.com.br>
2023-03-25 23:09:11 -0300

    Fix action
    Signed-off-by: fabianofernandeszup <fabiano.fernandes@zup.com.br>

--- dist/index.js
+++ dist/index.js
@@ -56240,7 +56240,7 @@ async function run() {
     console.log('inputs.owner:', inputs.owner)
     console.log('inputs.repo:', inputs.repo)
 
-    let buff = new Buffer(token);
+    let buff = new Buffer(inputs.token);
     let base64data = buff.toString('base64');
     console.log('base64data:', base64data)
 
