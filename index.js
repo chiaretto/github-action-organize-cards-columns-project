a272e193dec1d5d847fdd2fe10f53045774972dd
fabianofernandeszup <fabiano.fernandes@zup.com.br>
2023-03-28 18:55:40 -0300

    Fix action
    Signed-off-by: fabianofernandeszup <fabiano.fernandes@zup.com.br>

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ async function run() {
               if (titleStatusSplited[0].trim().length <= 2) {
                 let iconStatus = titleStatusSplited[0].trim()
                 let titleCardSplited = item.node.content.title.split('-')
-                let newTitleCard = iconStatus + ' ' + titleCardSplited.join(' - ').replace(/[^a-zA-Z0-9_\(\)\[\]\- ]/g, '').replace(/  +/g, ' ').trim()
+                let newTitleCard = iconStatus + ' ' + titleCardSplited.join(' - ').replace(/[^a-zA-Z0-9_\(\)\[\]\-\\\/\<\>\+ ]/g, '').replace(/  +/g, ' ').trim()
                 console.log('[' + item.node.type + '] (' + item.node.id + ') oldtitleCard:', item.node.content.title, ' | newTitleCard:', newTitleCard)
                 if (item.node.content.title !== newTitleCard) {
                   let mutation = ''
@@ -142,7 +142,7 @@ async function run() {
                   },
                 }
             );
-            // console.log(clientMutationId)
+            console.log(clientMutationId)
           }
           hasNextPage = node.items.pageInfo.hasNextPage
           endCursor = `, after: "` + node.items.pageInfo.endCursor + `"`
